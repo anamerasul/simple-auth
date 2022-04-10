2cb2a1a85fc413a8a4fff6935d47b85712c87506
anamerasul <anamerasul@gmail.com>
2022-04-11 03:15:41 +0600

    Sign in with google, open google login popup

--- simple-firebase-authtication/src/App.js
+++ simple-firebase-authtication/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import { app } from './firebase.init';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const auth = getAuth(app)
-function App() {
-  return (
-    <div className="">
 
+function App() {
+
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    console.log('working')
+
+    signInWithPopup(auth, provider)
+      .then(result => {
+
+        const user = result.user
+        console.log(user)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+
+  return (
+    <div className="App">
+      <button onClick={handleGoogleSignIn} className="rounded-full bg-orange-200 px-4 py-4 mt-12">Google sign in</button>
     </div>
   );
 }
--- simple-firebase-authtication/src/index.js
+++ simple-firebase-authtication/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOMClient from 'react-dom/client';
+// import ReactDOMClient from 'react-dom/client';
+
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
+// const root = ReactDOMClient.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
