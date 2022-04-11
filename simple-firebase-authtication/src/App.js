e02a899eccc0db046d30883ddd5fd7f56ccfba3b
anamerasul <anamerasul@gmail.com>
2022-04-11 22:34:34 +0600

    Display logged in user info, Sign out, toggle sign in, sign out

--- simple-firebase-authtication/src/App.js
+++ simple-firebase-authtication/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import { app } from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react'
 const auth = getAuth(app)
 
 function App() {
+
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user
+        setUser(user)
         console.log(user)
 
       })
@@ -18,9 +21,35 @@ function App() {
       })
   }
 
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+
+      })
+      .catch(error => {
+        setUser({})
+      })
+  }
+
+  console.log(user.photoURL)
+
   return (
+
+
     <div className="App">
-      <button onClick={handleGoogleSignIn} className="rounded-full bg-orange-200 px-4 py-4 mt-12">Google sign in</button>
+
+      {
+        user.uid ? <button onClick={handleGoogleSignOut} className="rounded-full bg-orange-200 px-4 py-4 mt-12">Google sign out</button> : <button onClick={handleGoogleSignIn} className="rounded-full bg-orange-200 px-4 py-4 mt-12">Google sign in</button>
+
+      }
+
+
+      <h2>{user.displayName}</h2>
+      <h2>{user.email}</h2>
+      <img src={user.photoURL} alt="" />
+
+
     </div>
   );
 }
